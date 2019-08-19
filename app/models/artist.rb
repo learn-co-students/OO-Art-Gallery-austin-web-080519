cc8cc333c631964c30bf4b83652bcd4e16dac746
Syd <green@Syds-MacBook-Air-2.local>
2019-08-19 09:43:47 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,41 @@
+require_relative 'gallery'
+require_relative 'painting'
+
 class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    my_paintings = Painting.all.select {|a| a == self}
+  end
+
+  # def galleries
+  #   my_galleries = Gallery.all.select {|a| a == self}
+  # end
+
+  def cities
+  end
+
+  def self.total_experience
+  end
+
+  def self.most_prolific
+  end
+
+  def create_painting(title, price, gallery)
+    new_painting = Painting.new(title, price, self, gallery)
   end
 
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,51 @@
+require_relative 'artist'
+require_relative 'painting'
+
 class Gallery
 
-  attr_reader :name, :city
+  attr_accessor :painting, :name, :city, :artist
+
+  @@all = []
 
   def initialize(name, city)
+    @painting = painting
+    @artist = artist
     @name = name
     @city = city
+    @@all << self
   end
+ 
+  def self.all
+    @@all
+  end
+
+  def add_painting(name)
+#find all the paintings and select the i want
+#reassign the painting to my gallery
+    find_painting_from_gallery = Painting.all.find {|p| p.title == name}
+    assign_painting_to_gallery = find_painting_from_gallery.gallery = self
+  end
+
+
+  def paintings
+    #find the paintings in the gallery by searching paintings where self is the gallery
+    my_paintings = Painting.all.select {|p| p.gallery == self}
+  end
+
+  def artists
+    #find the paintings in this gallery
+    my_paintings = Painting.all.select {|p| p.gallery == self}
+    #find the artists of those paintings
+    my_artists = my_paintings.map {|p| p.artist}
+
+  end
+
+  def artist_names
+  end
+
+  def most_expensive_painting
+  end
+  
+
 
 end
